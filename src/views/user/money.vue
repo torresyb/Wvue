08ae35adcc7df2461f0638d5bf758381f253a846
suifengcool <371801080@qq.com>
2017-07-19 00:30:15 +0800

    zgf

--- src/views/user/money.vue
+++ src/views/user/money.vue
@@ -13,27 +13,20 @@
                 <span>收支明细</span>
                 <i class="iconfont icon-right"></i>
             </li>
-            <li class="border-bottom" @click.native="showPop=true">
+            <li class="border-bottom" @click="show()">
                 <span>申请提现</span>
                 <i class="iconfont icon-right"></i>
             </li>
         </ul>
+        <div v-if="showPop" class="btn">
+            <input type="number" placeholder="请输入提现金额" v-model="amount"><i>元</i>
+            <button type="button" @click="getCash">确认提现</button>
+        </div>
         <h3>提现说明：</h3>
         <p>1.目前提现只支持提现到微信零钱</p>
         <p>2.提现日期固定为每周三，一周仅限一次</p>
         <p>3.若产生交易纠纷，客户投诉，该笔订单的费用会暂时被冻结，等待平台审核通过后方可提现</p>
         <p>4.对提现操作有任何疑问，请在工作日内(09:00-19:00)在公众号留言，会有专人第一时间为您处理</p>
-
-        <div v-model="showPop" class="get-cash-box" @click.self="showPop=false">
-            <div class="pop-box">
-                <h3>请输入提现金额</h3>
-                <input type="number">
-                <div class="btn">
-                    <button>取消</button>
-                    <button>确认</button>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
@@ -51,6 +44,7 @@ export default {
             config: vm.config,        // 配置
             deposit: null,            // 冻结金额
             total: null,              // 余额
+            amount: null,
             showPop: false
         }
     },
@@ -87,7 +81,33 @@ export default {
 
         // 收支明细
         getCash(){
-            this.$router.push('/user/getCash')
+            if(!amount){
+                this.$vux.toast.show({
+                    text: '请输入提现金额',
+                    type: 'text'
+                })
+                return
+            }
+            this.$http.get(`/user/account/getCash?amount=${this.amount}`)
+            .then(rst => {
+                if(rst.body && rst.body.res_code === 200){
+                    this.showPop = false
+                    this.$vux.toast.show({
+                        text: '申请提现成功',
+                        type: 'text'
+                    })
+                }
+            })
+            .catch(err => {
+                this.$vux.toast.show({
+                    text: err.body.msg,
+                    type: 'text'
+                })
+            })
+        },
+
+        show(){
+            this.showPop = !this.showPop
         }
     }
 }
@@ -141,5 +161,29 @@ export default {
                 line-height: 40px
                 &:first-child
                     border-right: 1px solid #dedede
-
+.btn
+    height: 200px
+    position: relative
+    input 
+        width: 50%
+        display: block
+        height: 45px
+        border: 1px solid #dedede
+        margin: 20px auto
+        text-align: center
+        outline: none
+    button
+        border: none
+        background: #03ca9d
+        color: #fff
+        height: 45px
+        margin: 0 auto
+        display: block
+        width: 92%
+        border-radius: 6px
+    i 
+        position: absolute
+        top: 13px
+        right: 68px
+        font-style: normal
 </style>
